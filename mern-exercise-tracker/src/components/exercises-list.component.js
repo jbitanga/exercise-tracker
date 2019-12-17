ec7bad8963f211e9f42c07ef24e64aa306002e11
Jared Bitanga <JaredAnthony.Bitanga@calbaptist.edu>
2019-12-17 13:31:09 -0800

    Finished projects / fixed routes

--- mern-exercise-tracker/src/components/exercises-list.component.js
+++ mern-exercise-tracker/src/components/exercises-list.component.js
@@ -1,80 +1,72 @@
-import React, { Component } from 'react'; // Always going to import component from react
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios'; 
+import axios from 'axios';
 
-
-
-const Exercise = props => ( //Use function component instead class component if just taking in props and returning JSX
-    <tr>
-        <td>{props.exerc.username}</td>
-        <td>{props.exerc.description}</td>
-        <td>{props.exerc.duration}</td>
-        <td>{props.exerc.date.substring(0, 10)}</td>
-        <td>
-        <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
-        </td>
-    </tr>
+const Exercise = props => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0,10)}</td>
+    <td>
+    <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+    </td>
+  </tr>
 )
 
-
-
 export default class ExercisesList extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.deleteExercise = this.deleteExercise.bind(this);
+    this.deleteExercise = this.deleteExercise.bind(this)
 
-        this.state = {exercises: []};
-    }
+    this.state = {exercises: []};
+  }
 
-    componentDidMount() {
-        axios.get('http://localhost:5000/exercises') 
-        .then(response => {
-            this.setState({ exercises: response.data}) 
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }
+  componentDidMount() {
+    axios.get('http://localhost:5000/exercises/')
+      .then(response => {
+        this.setState({ exercises: response.data })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
 
-    deleteExercise(Id) {
-        axios.delete('http:localhost:5000/exercises/' + id)
-        .then(res => console.log(res.data));
+  deleteExercise(id) {
+    axios.delete('http://localhost:5000/exercises/'+id)
+      .then(response => { console.log(response.data)});
 
-        this.setState({
-                        //Array of exercises       //Whenever the Id of the exercises does not equal the id that were deleting we pass it back to the exercise array
-            exercises: this.state.exercises.filter(el => el._id !== id)
-        })
-    }
+    this.setState({
+      exercises: this.state.exercises.filter(el => el._id !== id)
+    })
+  }
 
-    exerciseList(){
-        return this.state.exercises.map(currentExercise => {
-            //Returns component (row of table)
-            return <Exercise exercise={currentExercise} deleteExercise={this.deleteExercise} key={currentExercise._id}/>;
-        })
-    }
+  exerciseList() {
+    return this.state.exercises.map(currentexercise => {
+      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
+    })
+  }
 
-    render() {
-        return (
-            <div>
-                <h3>Logged Exercises</h3>
-                <table className="table">
-                    <thead className="thead=light">
-                        <tr>
-                            <th>Username</th>
-                            <th>Description</th>
-                            <th>Duration</th>
-                            <th>Date</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { this.ExercisesList() }
-                    </tbody>
-                </table>
-            </div>
-         
-
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.exerciseList() }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
